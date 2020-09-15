67a45a3349df185313d4eb8a0944a24693b0856f
vgiribabu <58927509+vgiribabu@users.noreply.github.com>
2020-09-15 13:14:33 +0530

    my second commit

--- recipe/urls.py
+++ recipe/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path('index/', views.index, name='index'),
     path('<int:recipe_id>', views.detail, name='detail'),
     path('create/', views.create, name='create'),
+    path('', views.login_view, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout, name='logout'),
 ]
+# if settings.DEBUG:
+#         urlpatterns += static(settings.MEDIA_URL,
+ #                               document_root=settings.MEDIA_ROOT)
--- recipe/views.py
+++ recipe/views.py
@@ -1,9 +1,44 @@
 from django.shortcuts import render, get_object_or_404
 from recipe.models import Recipe
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.utils import timezone
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+
+def login_view(request):
+    if request.method == 'POST':
+        # import pdb;pdb.set_trace()
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('recipe:index'))
+        else:
+            return HttpResponse('Invalid credentials')
+    return render(request, 'recipe/login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        User.objects.create_user(
+            username = request.POST['username'],
+            password = request.POST['password'],
+            email = request.POST['email'],
+        )
+        return HttpResponseRedirect(reverse('recipe:login'))
+    return render(request, 'recipe/register.html')
+
+
+def logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('recipe:login'))
+
 
 def index(request):
     recipe = Recipe.objects.all()
@@ -16,7 +51,7 @@ def detail(request, recipe_id):
     return render(request, "recipe/detail.html", {'check': check})
 
 
-
+@login_required(login_url='/recipe/login')
 def create(request):
     if request.method == "POST":
         Recipe.objects.create(
